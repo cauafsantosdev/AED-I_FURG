785330182e9f50370a14e5ec711d44eb627e4a9e
cauafsantosdev <cauafsantosdev@gmail.com>
2024-09-18 16:31:09 -0300

    commiting beecrowd exercises done in class

--- Beecrowd/1278.py
+++ Beecrowd/1278.py
@@ -1,25 +1,28 @@
+lista = []
+espacos = []
+
 while True:
     n = int(input())
-
     if n == 0:
         break
-
     else:
-        lista = []
         temporario = []
+        caracteres = 0
 
         for i in range(n):
-            frase = input()
+            frase = input().strip()
             temporario.append(" ".join(frase.split()))
 
         lista.append(temporario)
-        
-        espacos = []
         caracteres = len(max(temporario, key=len))
         espacos.append(caracteres)
-            
-        for i in range(n):
-            frase = lista[i]
-            print(f"{frase.rjust(caracteres)}")
-        
-        print()
+
+
+for idx, texto in enumerate(lista):
+    justificar = espacos[idx]
+    
+    for frase in texto:
+        print(f"{frase.rjust(justificar)}")
+
+    if idx < len(lista) - 1:
+        print() 
